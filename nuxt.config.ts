2060636aad7d0ee7ee57ba99b0dc629c363a9fb4
Lamarc <lamarcplaybr@gmail.com>
2026-08-15 23:52:50 -0300

    style: fonte Inter + cores no estilo webminisite (texto intro limpo)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,12 @@ export default defineNuxtConfig({
         { name: 'theme-color', content: '#0a0a0c' }
       ],
       link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
+        },
         { rel: 'dns-prefetch', href: 'https://t.me' },
         { rel: 'dns-prefetch', href: 'https://privsex.com' },
         { rel: 'preconnect', href: 'https://t.me', crossorigin: 'anonymous' },
